13671e2021ee6da1f57fa856336c68b1e8fdd288
malikshubham827 <malikshubham827@gmail.com>
2017-01-29 00:34:15 +0530

    Add tests for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -272,3 +272,62 @@ describe('POST /users', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login the user and return the token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User
+          .findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0])
+              .toInclude({
+                access: 'auth',
+                token: res.headers['x-auth']
+              });
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it('should return 400 for invalid credentials', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + 'a'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User
+          .findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens.length)
+              .toBe(0)
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+});
